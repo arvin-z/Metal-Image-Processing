205d5cac881eb024e6ca7f362a9d0fbd7101b8f9
Arvin Z <arvinz2k@gmail.com>
2025-04-16 18:06:58 -0700

    Initial Commit

---
+++ GPUProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GPUProject
+//
+//  Created by Arvin Ziaei on 2025-04-16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
